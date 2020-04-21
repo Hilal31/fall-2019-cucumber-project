f8fa79aa74c6b0f9d6bd6b2516bfe3ac24c02284
Hilal31 <g.iclal91@gmail.com>
2020-04-20 23:39:41 -0400

    first com

--- src/test/java/com/cybertek/library/runners/CukesRunner.java
+++ src/test/java/com/cybertek/library/runners/CukesRunner.java
@@ -6,7 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/recources/features"
+        features = "src/test/recources/features",
+        //to specify location of file use glue and paste from step definitions class
+        glue = "com/cybertek/library/step_definitions",
+        //
+        dryRun = true
 
 )
 
--- src/test/java/com/cybertek/library/step_definitions/LoginStepDefs.java
+++ src/test/java/com/cybertek/library/step_definitions/LoginStepDefs.java
@@ -10,18 +10,23 @@ public class LoginStepDefs {
     @Given("I am on the login page")
     public void i_am_on_the_login_page() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Going to login page");
     }
 
     @When("I login as admin user")
     public void i_login_as_admin_user() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Logging in as a librarian");
     }
     @Then("dashboard should be displayed")
     public void dashboard_should_be_displayed() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Verifying dashboard page");
     }
 
+    @When("I login as a student")
+    public void i_login_as_a_student() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("Logging as a student");
+    }
 }
